636db13351ca6f89dc0de8cee806c352fe04a125
Lightech <lightech@outlook.com>
2024-11-17 18:47:47 +0700

    Add basic API

--- mpu6050/include/mpu6050.h
+++ mpu6050/include/mpu6050.h
@@ -1,5 +1,72 @@
+/**
+ * This file defines the API to access the MPU6050.
+ */
+
 #ifndef _MPU6050_H_
 #define _MPU6050_H_
+
+/// @brief Structure to store the state of the MPU
+typedef struct {
+    /// @brief Raw reading of the accelerometer
+    int16_t accel_raw[3];
+
+    /// @brief Raw reading of the gyroscope
+    int16_t gyro_raw[3];
+
+    /// @brief Raw reading of the temperature
+    int16_t temp_raw;
+
+    /// @brief Gyroscope offsets (calibrated) to adjust the raw values
+    /// @todo Do we need float here?
+    int16_t gyro_calib[3];
+
+    // Factor to multiply the raw gyroscope reading to get physical angular velocity (in degrees per seconds)
+    float gyro_conv_factor;
+
+    /// @brief Acceleration calibrated offset
+    /// @todo Do we need float here?
+    int16_t accel_calib[3];
+
+    /// @brief Factor to multiply the raw accelerometer reading to get physical acceleration (in g)
+    float accel_conv_factor;
+
+} mpu6050_t;
+
+/// @brief Initialize the MPU6050 module
+/// @return HAL I2C status
+/// @todo Add configuration options such as scale (range), ...
+HAL_StatusTypeDef mpu6050_init(mpu6050_t *mpu);
+
+/// @brief Read the raw accelerometer data (only) from the sensor
+/// The new reading is going to be updated in the input structure
+HAL_StatusTypeDef mpu6050_read_gyro_raw(mpu6050_t *mpu);
+
+/// @brief Read all sensor data (gyro, accel, temperature)
+HAL_StatusTypeDef mpu6050_read_accel_raw(mpu6050_t *mpu);
+
+/// @brief Read the raw gyroscope data (only) from the sensor
+HAL_StatusTypeDef mpu6050_read_all_raw(mpu6050_t *mpu);
+
+/// @brief Convert raw accelerometer data to physical acceleration
+/// @param accel A pointer to output three floats
+void mpu6050_get_physical_acceleration(mpu6050_t *mpu, float *accel);
+
+/// @brief Convert raw gyroscope data to physical angular velocity
+/// @param ang_velo A pointer to outut three floats
+void mpu6050_get_physical_angular_velocity(mpu6050_t *mpu, float *ang_velo);
+
+/// @brief Convert raw sensor data to temperature in degree Celsius
+/// @param temp A pointer to one float
+void mpu6050_get_physical_temperature(mpu6050_t *mpu, float *temp);
+
+/// @brief Calibrate the gyroscope
+HAL_StatusTypeDef mpu6050_calibrate_gyro(mpu6050_t *mpu);
+
+/// @brief Calibrate the accelerometer
+HAL_StatusTypeDef mpu6050_calibrate_accelerometer(mpu6050_t *mpu);
+
+/// @brief Read all sensor data and adjust them by the calibration
+HAL_StatusTypeDef mpu6050_read_all_calib(mpu6050_t *mpu);
 
 #endif
 
--- nrf24/include/nrf24.h
+++ nrf24/include/nrf24.h
@@ -1,5 +1,56 @@
+/**
+ * This file defines the API to access the NRF24L01+.
+ *
+ * The main use cases considered will be
+ *  1. unidirectional communication (sender -> receiver); and
+ *  2. bidirectional peer-to-peer communication.
+ */
+
 #ifndef _NRF24_H_
 #define _NRF24_H_
+
+/// @brief Structure for the state of the device for troubleshooting,
+/// maintaining statistical data about the quality of service (number
+/// of packets successfully sent/dropped), latency, etc.
+typedef struct {
+    /// @brief The current number of packets in the TX buffer
+    uint8_t tx_fifo_size;
+
+    /// @brief The current number of packets in the RX buffer
+    uint8_t rx_fifo_size;
+
+    /// @brief The device status, can be used to determine the IRQ kind
+    uint8_t status;
+
+    /// @brief The FIFO status
+    uint8_t fifo_status;
+} nrf24_t;
+
+/// @brief Initialize the NRF24 unit
+/// @todo Add configuration
+void nrf24_init(nrf24_t *rf);
+
+/// @brief Update the status of the device
+void nrf24_get_status(nrf24_t *rf);
+
+/// @brief Update the FIFO status of the device
+void nrf24_get_fifo_status(nrf24_t *rf);
+
+/// @brief Read a packet arrived in the RX FIFO
+/// @param buffer Buffer provided by client to store the data 
+/// @param length To contain the size of the data read
+void nrf24_receive(nrf24_t *rf, uint8_t* buffer, uint8_t *length);
+
+/// @brief For primary transmitter (PTX) node, put a packet into the TX FIFO for transmission
+/// @param payload The payload to transmit
+/// @param length The length of the payload message
+void nrf24_transmit(nrf24_t *rf, uint8_t* payload, uint8_t length);
+
+/// @brief For primary receiver (PRX) node, put an acknowledgement payload into the TX FIFO
+/// to transmit back to the transmitter (PTX) on the arrival of the NEXT packet.
+/// @param ack The acknowledgement payload
+/// @param length Length of the acknowledgement
+void nrf24_write_ack(nrf24_t *rf, uint8_t* ack, uint8_t length);
 
 #endif
 
